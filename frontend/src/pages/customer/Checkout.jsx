ada85a98a671085729ed1e5e419f0d7472706ec5
shadeep0918 <shadeepisuru12345@gmail.com>
2026-07-12 03:01:03 +0530

    import relavent things and initiate the checkout function

--- frontend/src/pages/customer/Checkout.jsx
+++ frontend/src/pages/customer/Checkout.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useCart } from '../../context/CartContext';
+
+
 export default function Checkout() {
-  return (
-    <div>
-      <h1>Checkout Page</h1>
-    </div>
-  );
-}
+  const { cartItems, clearCart } = useCart();
+  const navigate = useNavigate();
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const subtotal = cartItems.reduce((sum, item) => sum + (item.price * item.quantity), 0);
